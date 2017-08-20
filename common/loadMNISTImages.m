5ef5e169fb66150c98cf7287c63f9c360806c40f
Robin Luo <vortune@163.com>
2017-08-20 14:21:57 +0800

    close file i/o immediately after loading images data

--- common/loadMNISTImages.m
+++ common/loadMNISTImages.m
@@ -13,10 +13,11 @@ numRows = fread(fp, 1, 'int32', 0, 'ieee-be');
 numCols = fread(fp, 1, 'int32', 0, 'ieee-be');
 
 images = fread(fp, inf, 'unsigned char');
-images = reshape(images, numCols, numRows, numImages);
-images = permute(images,[2 1 3]);
 
 fclose(fp);
+
+images = reshape(images, numCols, numRows, numImages);
+images = permute(images,[2 1 3]);
 
 % Reshape to #pixels x #examples
 images = reshape(images, size(images, 1) * size(images, 2), size(images, 3));
